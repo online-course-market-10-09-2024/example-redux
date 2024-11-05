e6c57c5d930d488c3b3dc9c1b7f51cbd7bdd0b72
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-11-05 15:08:15 +0700

    Clean code react-admin-component

--- react-admin-component/src/components/Menu.jsx
+++ react-admin-component/src/components/Menu.jsx
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Menu.css'; // Import file CSS
 
 const Menu = ({ userState, onMenuChange }) => {
-    const handleButtonClick = (status) => {
-        onMenuChange(status);
-    };
+  const handleButtonClick = (status) => {
+    onMenuChange(status);
+  };
 
-    return (
-        <div className='button-container'>
-            <button
-                className={`menu-button ${userState === 'active' ? 'selected' : ''}`}
-                onClick={() => handleButtonClick('active')}
-            >
-                Người dùng đang hoạt động
-            </button>
-            <button
-                className={`menu-button ${userState === 'banned' ? 'selected' : ''}`}
-                onClick={() => handleButtonClick('banned')}
-            >
-                Người dùng bị cấm
-            </button>
-        </div>
-    );
+  return (
+    <div className='button-container'>
+      <button
+        className={`menu-button ${userState === 'active' ? 'selected' : ''}`}
+        onClick={() => handleButtonClick('active')}
+      >
+        Người dùng đang hoạt động
+      </button>
+      <button
+        className={`menu-button ${userState === 'banned' ? 'selected' : ''}`}
+        onClick={() => handleButtonClick('banned')}
+      >
+        Người dùng bị cấm
+      </button>
+    </div>
+  );
 };
 
 export default Menu;
--- react-admin-component/src/components/SearchBar.jsx
+++ react-admin-component/src/components/SearchBar.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import './SearchBar.css'
 
 const SearchBar = ({ onSearchChange }) => {
-    const handleInputChange = (e) => {
-        onSearchChange(e.target.value);
-    };
+  const handleInputChange = (e) => {
+    onSearchChange(e.target.value);
+  };
 
-    return (
-        <div>
-            <input
-                className='search-input'
-                type="text"
-                placeholder="Tìm kiếm người dùng..."
-                onChange={handleInputChange}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <input
+        className='search-input'
+        type="text"
+        placeholder="Tìm kiếm người dùng..."
+        onChange={handleInputChange}
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
